f83d5ae84c3b9d9abd6fb5da7ea1d2259770bfa0
Michał Pawełczyk <michal@localhost.localdomain>
2020-04-11 09:16:46 +0200

    init project commit. Closes #1.wq

---
+++ src/main/java/pro/pawelczyk/pppetclinic/PpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.pppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/pawelczyk/pppetclinic/PpPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.pppetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
