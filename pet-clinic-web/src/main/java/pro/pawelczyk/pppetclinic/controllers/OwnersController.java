a1a73100829e10ab0fe224743d910d8c58f37e38
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2020-04-14 10:53:31 +0200

    list owners on web page - owners list. closes #19

--- pet-clinic-web/src/main/java/pro/pawelczyk/pppetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/pro/pawelczyk/pppetclinic/controllers/OwnersController.java
@@ -1,7 +1,9 @@
 package pro.pawelczyk.pppetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.pawelczyk.pppetclinic.services.OwnerService;
 
 /**
  * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
@@ -12,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    String listOwners() {
+    String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
