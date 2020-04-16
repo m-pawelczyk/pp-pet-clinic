ed3ab6720a68dd6ba4489402b90e5f85b5b427fb
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2020-04-16 16:13:29 +0200

    speciality map service implementation. Closes #34

---
+++ pet-clinic-data/src/main/java/pro/pawelczyk/pppetclinic/services/SpecialityService.java
@@ -0,0 +1,12 @@
+package pro.pawelczyk.pppetclinic.services;
+
+import pro.pawelczyk.pppetclinic.model.Speciality;
+
+/**
+ * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
+ * on 16.04.2020
+ * created SpecialietesService in pro.pawelczyk.pppetclinic.services
+ * in project pp-pet-clinic
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/pro/pawelczyk/pppetclinic/services/map/SpecialityMapServiceImpl.java
@@ -0,0 +1,41 @@
+package pro.pawelczyk.pppetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pro.pawelczyk.pppetclinic.model.Speciality;
+import pro.pawelczyk.pppetclinic.services.SpecialityService;
+
+import java.util.Set;
+
+/**
+ * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
+ * on 16.04.2020
+ * created SpecialityMapServiceImpl in pro.pawelczyk.pppetclinic.services.map
+ * in project pp-pet-clinic
+ */
+@Service
+public class SpecialityMapServiceImpl extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
