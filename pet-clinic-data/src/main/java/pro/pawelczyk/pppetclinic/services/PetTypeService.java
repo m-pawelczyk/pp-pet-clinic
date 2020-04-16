c396c14f9623d782e688f180b862eda98924bb7e
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2020-04-16 13:25:53 +0200

    PetType service implementation. Closes #32

---
+++ pet-clinic-data/src/main/java/pro/pawelczyk/pppetclinic/services/PetTypeService.java
@@ -0,0 +1,12 @@
+package pro.pawelczyk.pppetclinic.services;
+
+import pro.pawelczyk.pppetclinic.model.PetType;
+
+/**
+ * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
+ * on 16.04.2020
+ * created PetTypeService in pro.pawelczyk.pppetclinic.services
+ * in project pp-pet-clinic
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
