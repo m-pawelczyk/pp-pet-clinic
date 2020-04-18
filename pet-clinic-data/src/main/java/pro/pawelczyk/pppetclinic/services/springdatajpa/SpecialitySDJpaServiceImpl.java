e671bb4f606d39bb6b7bb90a7a3c209065108a4f
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2020-04-18 16:17:05 +0200

    implementation spring data jpa for speciality. closes #47

---
+++ pet-clinic-data/src/main/java/pro/pawelczyk/pppetclinic/services/springdatajpa/SpecialitySDJpaServiceImpl.java
@@ -0,0 +1,55 @@
+package pro.pawelczyk.pppetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import pro.pawelczyk.pppetclinic.model.Speciality;
+import pro.pawelczyk.pppetclinic.repositories.SpecialityRepository;
+import pro.pawelczyk.pppetclinic.services.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
+ * on 18.04.2020
+ * created SpecialitySDJpaServiceImpl in pro.pawelczyk.pppetclinic.services.springdatajpa
+ * in project pp-pet-clinic
+ */
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaServiceImpl implements SpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaServiceImpl(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+
+        Set<Speciality> specialities = new HashSet<>();
+
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
