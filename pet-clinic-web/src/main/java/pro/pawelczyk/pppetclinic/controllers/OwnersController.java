8d6735fbe3d44f85950b7dfd65a4a8d2bb778c2c
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2020-05-02 15:49:58 +0200

    show owners details - first step

--- pet-clinic-web/src/main/java/pro/pawelczyk/pppetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/pro/pawelczyk/pppetclinic/controllers/OwnersController.java
@@ -2,7 +2,10 @@ package pro.pawelczyk.pppetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import pro.pawelczyk.pppetclinic.services.OwnerService;
 
 /**
@@ -32,5 +35,12 @@ public class OwnersController {
     public String findOwners() {
 
         return "notimplemented";
+    }
+
+    @GetMapping("/owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
