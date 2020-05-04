53e32f2d72365bf17f08bc295ad9e35e2e8f54f6
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2020-05-04 17:18:13 +0200

    implement JSON endpoint for vets. closes #68

--- pet-clinic-web/src/main/java/pro/pawelczyk/pppetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pro/pawelczyk/pppetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package pro.pawelczyk.pppetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pro.pawelczyk.pppetclinic.model.Vet;
 import pro.pawelczyk.pppetclinic.services.VetService;
+
+import java.util.Set;
 
 /**
  * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
@@ -26,5 +31,9 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets") public @ResponseBody Set<Vet> getJson() {
+        return vetService.findAll();
     }
 }
